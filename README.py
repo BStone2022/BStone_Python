cce3863dcaba4ccbe974b323c1ac004d36bdfac7
BenStone2023 <114768584+BStone2022@users.noreply.github.com>
2023-08-08 15:32:39 -0700

    Create README.py

---
+++ README.py
@@ -0,0 +1,6 @@
+# Credit Card Fraud Detection Models Using Machine Learning
+
+Dataset: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud?sort=votes
+
+This repo gives python code that builds several machine learning models for detecting fraud based on customer characteristics. 
+  Included models: K-Nearest Neighbors (KNN), Support Vector Machine (SVM), Random Forest Classifier, Neural Network
